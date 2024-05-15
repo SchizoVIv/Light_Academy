d0cdbf14917814e132f9e945ca5f2382b7ed9b87
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-05-15 20:00:01 +0300

    feat: change burger, add adaptive

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -14,48 +14,7 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   });
 });
 
-
 //
-
-// const slider = document.querySelector(".items");
-// 		const slides = document.querySelectorAll(".item");
-// 		const button = document.querySelectorAll(".button");
-
-// 		let current = 0;
-// 		let prev = 4;
-// 		let next = 1;
-
-// 		for (let i = 0; i < button.length; i++) {
-// 			button[i].addEventListener("click", () => i == 0 ? gotoPrev() : gotoNext());
-// 		}
-
-// 		const gotoPrev = () => current > 0 ? gotoNum(current - 1) : gotoNum(slides.length - 1);
-
-// 		const gotoNext = () => current < 4 ? gotoNum(current + 1) : gotoNum(0);
-
-// 		const gotoNum = number => {
-// 			current = number;
-// 			prev = current - 1;
-// 			next = current + 1;
-
-// 			for (let i = 0; i < slides.length; i++) {
-// 				slides[i].classList.remove("active");
-// 				slides[i].classList.remove("prev");
-// 				slides[i].classList.remove("next");
-// 			}
-
-// 			if (next == 5) {
-// 				next = 0;
-// 			}
-
-// 			if (prev == -1) {
-// 				prev = 4;
-// 			}
-
-// 			slides[current].classList.add("active");
-// 			slides[prev].classList.add("prev");
-// 			slides[next].classList.add("next");
-// 		}
 
 const sliderList = document.querySelectorAll('.slider__nav');
 const cardList = document.querySelectorAll('.reviews__images');
@@ -94,3 +53,144 @@ inputs.forEach(function(input, index) {
 });
 
 
+// faq
+
+// const items = document.querySelectorAll(".accordion button");
+
+// function toggleAccordion() {
+//   const itemToggle = this.getAttribute('aria-expanded');
+//   console.log(itemToggle)
+
+//   for (i = 0; i < items.length; i++) {
+//     items[i].setAttribute('aria-expanded', 'false');
+//   }
+
+//   if (itemToggle == 'false') {
+//     this.setAttribute('aria-expanded', 'true');
+//   }
+// }
+
+// items.forEach(item => item.addEventListener('click', toggleAccordion));
+
+// const items = document.querySelectorAll('.slider__nav')
+// const slider = document.querySelector('.slider__inner')
+// const wingLeft = document.querySelector('.reviews__wing-left')
+// const wingRight = document.querySelector('.reviews__wing-right')
+
+// function handleChangeCardLeft() {
+
+//   console.log(slider.style.left)
+// }
+
+// wingRight.addEventListener('click', handleChangeCardLeft)
+
+
+// burger
+
+const burgerToggle = document.querySelector('#menu-toggle')
+const burgerButton = document.querySelector('.header__button-container')
+const burgerOverflow = document.querySelector('.header__overflow')
+const navigation = document.querySelector('.header__nav-box')
+const contacts = document.querySelector('.header__contacts')
+
+function hendleOpenMenu() {
+  console.log(burgerOverflow)
+  if(!burgerToggle.checked) {
+    burgerOverflow.classList.remove('overflow_hidden')
+    navigation.classList.add('header__nav-box_active')
+    contacts.classList.add('header__contacts_active')
+    disableScroll();
+  } else {
+    enableScroll()
+    burgerOverflow.classList.add('overflow_hidden')
+    navigation.classList.remove('header__nav-box_active')
+    contacts.classList.remove('header__contacts_active')
+  }
+}
+
+function disableScroll() {
+  // Сохраняем текущую позицию скролла
+  const scrollY = window.scrollY;
+  // Добавляем стили для блокировки скролла
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollY}px`;
+  document.body.style.width = '100%';
+}
+
+function enableScroll() {
+  // Восстанавливаем позицию скролла
+  const scrollY = parseInt(document.body.style.top, 10);
+  document.body.style.position = '';
+  document.body.style.top = '';
+  document.body.style.width = '';
+  window.scrollTo(0, Math.abs(scrollY));
+}
+
+// липкое меню
+
+const header = document.querySelector('.header')
+const headerLogo = document.querySelector('.header__logo')
+let lastScroll = 0;
+const defaultOffset = 150;
+
+const scrollPosition = () => window.pageXOffset || document.documentElement.scrollTop
+const conteinHide = () => header.classList.contains('header_hide')
+const conteinBorder = () => header.classList.contains('header_border')
+
+window.addEventListener('scroll', () => {
+  console.log(`${scrollPosition()} \ ${lastScroll}`)
+
+  if(scrollPosition() < lastScroll && conteinBorder() && scrollPosition() < defaultOffset) {
+    console.log('+')
+    header.classList.remove('header_hide')
+    header.classList.remove('header_border')
+    headerLogo.classList.remove('header__logo_hide')
+
+  }
+
+  if(scrollPosition() < lastScroll && conteinHide()) {
+    console.log('+')
+    header.classList.remove('header_hide')
+    header.classList.add('header_border')
+    headerLogo.classList.add('header__logo_hide')
+  }
+
+  if(scrollPosition() > lastScroll && !conteinHide() && scrollPosition() > defaultOffset) {
+    console.log('-')
+    header.classList.add('header_hide')
+    headerLogo.classList.add('header__logo_hide')
+  }
+
+  lastScroll = scrollPosition()
+})
+
+
+
+
+burgerButton.addEventListener('click', hendleOpenMenu)
+
+// window
+
+const popupButtonClose = document.querySelector('.popup__button-close')
+const popupOverflow = document.querySelector('.popup__overflow')
+const popupBox = document.querySelector('.popup__box')
+const popup = document.querySelector('.popup')
+const academyButton = document.querySelector('.academy__button')
+const presentButton = document.querySelector('.advantages__button')
+const coachingButton = document.querySelector('.coaching__button')
+const coachButton = document.querySelector('.coach__button')
+
+function popupHandleClose() {
+  popup.classList.add('popup__hidden')
+  console.log('hi')
+}
+
+function popupHandleOpen() {
+  popup.classList.remove('popup__hidden')
+}
+
+popupButtonClose.addEventListener('click', popupHandleClose)
+academyButton.addEventListener('click', popupHandleOpen)
+presentButton.addEventListener('click', popupHandleOpen)
+coachingButton.addEventListener('click', popupHandleOpen)
+coachButton.addEventListener('click', popupHandleOpen)
