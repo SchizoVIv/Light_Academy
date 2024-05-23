df9a820682d03f2205330a86a9957749505191ef
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-05-23 17:00:20 +0300

    feat: add changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -133,6 +133,34 @@ function enableScroll() {
   window.scrollTo(0, Math.abs(scrollY));
 }
 
+const anchorMenu = document.querySelector('.header__nav-box');
+
+anchorMenu.addEventListener('click', function(event) {
+  // Проверяем, что клик был на ссылке
+  if (event.target.tagName === 'A') {
+    // Получаем атрибут href и проверяем, что это якорная ссылка
+    const href = event.target.getAttribute('href');
+    if (href.charAt(0) === '#') {
+      // Получаем элемент на который ссылается якорь
+      const targetElement = document.querySelector(href);
+      if (targetElement) {
+        // Прокручиваем страницу к этому элементу
+        enableScroll()
+        targetElement.scrollIntoView({
+          behavior: 'smooth' // Плавная прокрутка
+        });
+        // Закрываем бургерное меню, если оно открыто
+        burgerToggle.checked = false;
+        burgerOverflow.classList.add('overflow_hidden')
+        navigation.classList.remove('header__nav-box_active')
+        contacts.classList.remove('header__contacts_active')
+        // Предотвращаем стандартное действие якорной ссылки
+        event.preventDefault();
+      }
+    }
+  }
+});
+
 // липкое меню
 
 const header = document.querySelector('.header')
@@ -268,10 +296,12 @@ const coachingButton = document.querySelector('.coaching__button')
 const coachButton = document.querySelector('.coach__button')
 
 function popupHandleClose(pop) {
+  enableScroll()
   pop.classList.add('popup__hidden')
 }
 
 function popupHandleOpen(pop) {
+  disableScroll()
   pop.classList.remove('popup__hidden')
 }
 
