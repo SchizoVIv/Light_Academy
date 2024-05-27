369d7a9770be6aba38354cae6ab4a3868ea31a6d
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-05-27 20:47:05 +0300

    feat: add new clider

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -695,17 +695,17 @@ if(screenWidthOver < 993) {
 
 //
 
-const swiper = new Swiper('.sample-slider', {
-  loop: true,
-  // autoplay: {
-  //   delay: 2000,
-  // },
-  navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-  },
-  pagination: {
-    el: '.swiper-pagination',
-},
-})
+// const swiper = new Swiper('.sample-slider', {
+//   loop: true,
+//   // autoplay: {
+//   //   delay: 2000,
+//   // },
+//   navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//   },
+//   pagination: {
+//     el: '.swiper-pagination',
+// },
+// })
 
